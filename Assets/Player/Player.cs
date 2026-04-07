236ab70fe602a27ddfc57626fa490d40d86b4efe
Nadinda <nadinda@nadinda.dev>
2026-04-07 15:57:07 +1000

    Add ThirdPersonCamera with Cinemachine

--- Assets/Player/Player.cs
+++ Assets/Player/Player.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] private float _speed;
+    [SerializeField] private Transform _camera;
     private Rigidbody _rigidBody;
  
     private void Awake()
@@ -12,11 +13,23 @@ public class Player : MonoBehaviour
         _rigidBody = GetComponent<Rigidbody>();
     }
     
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+    
     void Update()
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
-        Vector3 movementDirection = new Vector3(horizontal,0,vertical);
+        
+        Vector3 horizontalDirection = horizontal * _camera.right;
+        Vector3 verticalDirection = vertical * _camera.forward;
+        verticalDirection.y = 0;
+        horizontalDirection.y = 0;
+        
+        Vector3 movementDirection = horizontalDirection + verticalDirection;
         _rigidBody.velocity = movementDirection * _speed * Time.fixedDeltaTime;
     }
 }
